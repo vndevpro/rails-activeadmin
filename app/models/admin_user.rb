b9998f741c9b64607ff610c88979dcb2b29c6452
Juan <juanonsoftware@gmail.com>
2024-11-15 14:23:38 +0700

    Install activeadmin: fix Ransack's issue

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ApplicationRecord
+  include RansackSearchable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
